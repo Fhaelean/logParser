6849e1224ee53f2b210d730dc7f54d8f9b1a5bcc
Fhaelean <Fhaelean>
2019-06-04 15:27:16 +0300

    Added func to count remote host activity.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,8 @@ import java.sql.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         String logFile = "/Users/Fhaelean/Desktop/access.log";
         File file = new File(logFile);
         Scanner scanner;
@@ -15,7 +16,8 @@ public class Main {
 
         InsertRecords app = new InsertRecords();
 
-        try {
+        try
+        {
             scanner = new Scanner(file);
             int i = 0;
             while (/*scanner.hasNextLine()*/ i < 30)//TODO
@@ -23,10 +25,48 @@ public class Main {
                 app.insert(scanner.next(), scanner.next(), scanner.next(), scanner.next(), scanner.next(), scanner.next(), scanner.next(), scanner.next(), scanner.next(), scanner.next());
                 i++;
             }
-        } catch (Exception e) {
+        }
+        catch (Exception e)
+        {
             e.printStackTrace();
         }
 
+        try {
+            getRemHostRequsets();
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
 
     }
+
+    private static void getRemHostRequsets() throws SQLException
+    {
+        String url = "jdbc:sqlite:/Users/Fhaelean/Desktop/log/lab1.db";
+        Connection conn = DriverManager.getConnection(url);
+
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery(remHosts);
+
+        TreeMap<Integer, String> hosts = new TreeMap<>();
+        while (rs.next())
+        {
+            hosts.put(rs.getInt("amount"), rs.getString("remotehost"));
+        }
+
+        Set set = hosts.entrySet();
+        Iterator iterator = set.iterator();
+
+        while(iterator.hasNext()) {
+            Map.Entry map = (Map.Entry)iterator.next();
+            System.out.println("Remote host: " + map.getValue() + " request amount: " + map.getKey());
+        }
+    }
+
+    private static final String remHosts =
+            "SELECT COUNT(*) amount,remotehost\n" +
+            "FROM logData\n" +
+            "GROUP BY remotehost\n" +
+            "ORDER BY amount DESC\n" +
+            "LIMIT 10 OFFSET 0";
 }
